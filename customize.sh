538a34aa7d2f4fac5590b9ecf476cdeace787d70
TanakaLun <2709410138@qq.com>
2026-04-10 12:24:47 +0800

    refactor: Installation script logic

--- customize.sh
+++ customize.sh
@@ -1,13 +1,35 @@
 #!/system/bin/sh
-
-if [ -z "$KSU" ]; then
-    ui_print "! This module only supports KernelSU"
+if ! command -v ksud >/dev/null 2>&1; then
+    ui_print "! ksud command not found. This module requires ksud support."
     abort
 fi
 
-mv -f "$MODPATH/bin/ipset" "/data/adb/ksu/bin/"
-chmod 755 /data/adb/ksu/bin/ipset
-rm -rf "$MODPATH/bin"
+MODID=$(grep_prop id "$MODPATH/module.prop")
+if [ -z "$MODID" ]; then
+    ui_print "! Failed to read module ID."
+    abort
+fi
+
+TARGET_DIR="/data/modules/$MODID"
+mkdir -p "$TARGET_DIR"
+if [ -d "$MODPATH/bin" ]; then
+    cp -rf "$MODPATH/bin" "$TARGET_DIR/"
+else
+    ui_print "! bin folder not found in module."
+    abort
+fi
+
+set_perm_recursive "$TARGET_DIR/bin" 0 0 0755 0755
+
+if [ -L "/data/adb/ksu/bin/ipset" ]; then
+    rm -f "/data/adb/ksu/bin/ipset"
+elif [ -e "/data/adb/ksu/bin/ipset" ]; then
+    ui_print "! File exists at /data/adb/ksu/bin/ipset and is not a symlink"
+    abort
+fi
+ln -s "$TARGET_DIR/bin/ipset" "/data/adb/ksu/bin/ipset"
+
+ui_print "Binary Installation completed. Binary placed in $TARGET_DIR/bin"
 
 [ -d "$MODPATH/netfilter" ] && {
     rm -rf "/data/adb/netfilter"
